8c04018cf7a6d33521abe76184a9fca1f17788e1
1F10220266 <s1f102202662@iniad.org>
2023-06-22 15:52:35 +0900

    gcd ci-test(4)

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -20,7 +20,7 @@ class TestTarget(unittest.TestCase):
     
     def test_gcd_negative(self):
         self.assertEqual(gcd(-15, -25), 5)
-        self.assertEqual(gcd(-24, 32), 6)
+        self.assertEqual(gcd(-24, 32), 8)
 
     def test_gcd_zero(self):
         self.assertEqual(gcd(5, 0), 5)
