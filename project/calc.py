73d2215613805d28ebbb51005335e432215f7d6e
1F10220266 <s1f102202662@iniad.org>
2023-06-22 15:35:55 +0900

    gcd ci-test

--- project/calc.py
+++ project/calc.py
@@ -5,3 +5,20 @@ def fact(n):
     for i in range(1, n + 1):
         result *= i
     return result
+
+
+def gcd(a, b):
+    if a == 0 and b != 0:
+        return abs(b)
+    elif a != 0 and b == 0:
+        return abs(a)
+    elif a == 0 and b == 0:
+        return 0
+    
+    while b != 0:
+        r = a % b
+        if r == 0:
+            return b
+        else:
+            a = b
+            b = r
